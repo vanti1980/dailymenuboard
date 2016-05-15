870c80cb71093204595cbfbf470097eca00ea293
vanti1980 <vanti1980@gmail.com>
2016-05-15 11:40:31 +0200

    X-Domain requests work!!!!!

--- src/app/common/meal-set/meal-set.model.ts
+++ src/app/common/meal-set/meal-set.model.ts
@@ -4,8 +4,8 @@ import {Price} from '../meal/price.model';
  * The class describes a set of meals that is provided as one and cannot be bought separately.
  */
 export class MealSet {
-    constructor(name: string,
-        meals: Meal[],
-        price: Price) {
+    constructor(public name: string,
+        public meals: Meal[],
+        public price: Price) {
     }
 }
