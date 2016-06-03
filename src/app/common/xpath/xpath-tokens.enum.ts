2163db0d4083d7a761cef86042ee6fb74d08623e
vanti1980 <vanti1980@gmail.com>
2016-06-03 21:40:23 +0200

    Fix: Capitalized forms of days can be added as tokens, as well

--- src/app/common/xpath/xpath-tokens.enum.ts
+++ src/app/common/xpath/xpath-tokens.enum.ts
@@ -7,8 +7,26 @@ export class XpathTokens {
       return getNow().format("ddd");
     };
 
+    static dayAbbrOfWeekCap = function():string {
+      return getNow().format("ddd").toLocaleUpperCase();
+    };
+
+    static dayAbbrOfWeekCapFirst = function():string {
+      let str = getNow().format("ddd");
+      return str[0].toLocaleUpperCase() + str.substring(1);
+    };
+
     static dayOfWeek = function():string {
       return getNow().format("dddd");
+    };
+
+    static dayOfWeekCap = function():string {
+      return getNow().format("dddd").toLocaleUpperCase();
+    };
+
+    static dayOfWeekCapFirst = function():string {
+      let str = getNow().format("dddd");
+      return str[0].toLocaleUpperCase() + str.substring(1);
     };
 
     static numberOfWeekDay = function():string {
@@ -23,8 +41,26 @@ export class XpathTokens {
       return getNow().format("MMMM");
     };
 
+    static nameOfMonthCap = function():string {
+      return getNow().format("MMMM").toLocaleUpperCase();
+    };
+
+    static nameOfMonthCapFirst = function():string {
+      let str = getNow().format("MMMM");
+      return str[0].toLocaleUpperCase() + str.substring(1);
+    };
+
     static abbrOfMonth = function():string {
       return getNow().format("MMM");
+    };
+
+    static abbrOfMonthCap = function():string {
+      return getNow().format("MMM").toLocaleUpperCase();
+    };
+
+    static abbrOfMonthCapFirst = function():string {
+      let str = getNow().format("MMM");
+      return str[0].toLocaleUpperCase() + str.substring(1);
     };
 
     static numberOfMonth = function():string {
@@ -37,7 +73,7 @@ export class XpathTokens {
 
 
     public static values():string[] {
-      return ['dayAbbrOfWeek', 'dayOfWeek', 'numberOfWeekDay', 'dayOfMonth', 'nameOfMonth', 'abbrOfMonth', 'numberOfMonth', 'firstDayOfWeek'];
+      return ['dayAbbrOfWeek', 'dayAbbrOfWeekCap', 'dayAbbrOfWeekCapFirst', 'dayOfWeek', 'dayOfWeekCap', 'dayOfWeekCapFirst', 'numberOfWeekDay', 'dayOfMonth', 'nameOfMonth', 'nameOfMonthCap', 'nameOfMonthCapFirst', 'abbrOfMonth', 'abbrOfMonthCap', 'abbrOfMonthCapFirst', 'numberOfMonth', 'firstDayOfWeek'];
     }
 
     public static resolve(val:string):string {
