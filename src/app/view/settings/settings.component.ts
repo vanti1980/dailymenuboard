f540975c3b27b35b36d5fa54f4fc07cba138c5e9
vanti1980 <vanti1980@gmail.com>
2016-09-02 22:25:42 +0200

    Fix: Home marker not updated after changing data in Settings

--- src/app/view/settings/settings.component.ts
+++ src/app/view/settings/settings.component.ts
@@ -1,6 +1,7 @@
 import {NgForm, ControlArray, ControlGroup, Control, FormBuilder, Validators} from '@angular/common';
 import {Component} from '@angular/core';
 
+import {EmitterService, Events} from '../../common/event';
 import {MapService, MapComponent} from '../../common/map';
 
 import {GeoCodeResponseJSON, Location, Marker, IconType} from '../../common/map/map.model.ts';
@@ -15,17 +16,19 @@ export class SettingsComponent {
 
    public marker: Marker;
 
-   constructor(public mapService:  MapService){
+   constructor(
+     private emitterService: EmitterService,
+     public mapService:  MapService) {
       this.marker = this.mapService.getCachedHome();
    }
+
    public onSubmit() {
 
       this.mapService.getLocation(this.marker.address).subscribe((myLocation) => {
           this.marker.location = myLocation;
           this.mapService.cacheHome(this.marker);
+          this.emitterService.get(Events.HOME_CHANGED).emit(undefined);
       });
-
-        //this.mapComponent.ngOnInit()
   }
 
 }
